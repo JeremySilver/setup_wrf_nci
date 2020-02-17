e7fed7988b0efc99e15a8817b53971667dcdce1b
Jeremy Silver <jeremy.silver@unimelb.edu.au>
2020-02-17 14:58:01 +1100

    Updating shell environment modules to reflect those used in the COE CMS WRF 4.1.1 build

--- load_wrf_env.sh
+++ load_wrf_env.sh
@@ -2,9 +2,7 @@
 
 ## the following group should be the *same* as used in the WRF build.env files
 ## that contains the environment modules used in WRF.
-module load netcdf/4.7.1
+module load intel-compiler/2019.3.199
+module load openmpi/4.0.2
 module load hdf5/1.10.5
-module load intel-compiler/2019.5.281
-module load intel-mkl/2019.5.281
-module load intel-mpi/2019.5.281
-module load intel-ipp/2019.5.281
+module load netcdf/4.7.1
